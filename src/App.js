9a2f79e2221698e354617079de700aeac9179f81
chukwumaokere <chukwumaokere@yahoo.com>
2019-07-02 16:44:39 +0000

    styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import  FontAwesomeIcon  from '@fortawesome/react-fontawesome';
-import { faCheckSquare } from '@fortawesome/fontawesome-free-solid';
 import { Container, Row, Col } from 'react-bootstrap';
 import './Custom.css';
 
-library.add(faCheckSquare);
 
 class App extends Component {
   render() {
     return (
 	<div className="bg">
-		<Container>
 		 	<Row>
 		    	<Col> 
                     <div className="lp-left"> 
-                        Hi
+                        <div className="info-box mx-auto my-auto">
+                            <div className="info-box-item"> <span class="icon-box"> <i class="fa fa-2x fa-search icon-size"></i></span> Discover new artists and new music </div>
+                            <div className="info-box-item"> <span class="icon-box"> <i class="fa fa-2x fa-users icon-size"></i></span> Proudly share the artists you support </div>
+                            <div className="info-box-item"> <span class="icon-box"> <i class="fa fa-2x fa-seedling icon-size"></i></span> Help your favorite artists grow </div>
+                        </div>
                     </div> 
                 </Col>
 		 		<Col> 
@@ -25,7 +24,6 @@ class App extends Component {
                     </div> 
                 </Col>
 		  	</Row>
-		</Container>
 	</div>
     );
   }
